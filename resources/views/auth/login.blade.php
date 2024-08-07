a0f4e6294c43367bac1dcfe657469438e4cd949d
altamisatmaja <altamisfattah16@gmail.com>
2024-08-07 17:36:27 +0700

    feat: logins layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.guest.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400..700&display=swap" rel="stylesheet" />
-
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-
-<body class="font-inter antialiased bg-gray-100  text-gray-600 ">
+@section('content')
     <main class="bg-white ">
         <div class="relative flex">
             <div class="w-full md:w-1/2">
@@ -25,8 +8,8 @@
                     <div class="max-w-sm mx-auto w-full px-4 py-10">
                         <div class="flex items-center justify-center h-16 gap-y-10 sm:py-16 lg:py-20">
                             <a href="{{ route('login') }}" class="flex items-center gap-x-4">
-                                <svg xmlns="http://www.w3.org/2000/svg" height="75px" viewBox="0 -960 960 960"
-                                    width="75px" fill="#ea580c">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="75px" viewBox="0 -960 960 960" width="75px"
+                                    fill="#ea580c">
                                     <path
                                         d="M219-120q-25 0-42.5-17.5T159-180q0-25 17.5-42.5T219-240h64L181-574q-27-15-44.5-44T119-680q0-50 35-85t85-35q39 0 69.5 22.5T351-720h128v-40q0-17 11.5-28.5T519-800q9 0 17.5 4t14.5 12l68-64q9-9 21.5-11.5T665-856l156 72q12 6 16.5 17.5T837-744q-6 12-17.5 15.5T797-730l-144-66-94 88v56l94 86 144-66q11-5 23-1t17 15q6 12 1 23t-17 17l-156 74q-12 6-24.5 3.5T619-512l-68-64q-6 6-14.5 11t-17.5 5q-17 0-28.5-11.5T479-600v-40H351q-3 8-6.5 15t-9.5 15l200 370h84q25 0 42.5 17.5T679-180q0 25-17.5 42.5T619-120H219Zm20-520q17 0 28.5-11.5T279-680q0-17-11.5-28.5T239-720q-17 0-28.5 11.5T199-680q0 17 11.5 28.5T239-640Z" />
                                 </svg>
@@ -105,6 +88,4 @@
         </div>
 
     </main>
-</body>
-
-</html>
+@endsection
