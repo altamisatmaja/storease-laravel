73ecca41ebbfd9d4563f6026c3dec90a73964745
altamisatmaja <altamisfattah16@gmail.com>
2024-08-10 02:08:26 +0700

    feat: dynamic fe home

--- app/Http/Controllers/Guest/GuestHomePagesController.php
+++ app/Http/Controllers/Guest/GuestHomePagesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use App\Models\AboutPages;
 use App\Models\Hero;
+use App\Models\PortofolioHomePages;
 use App\Models\TestimonialHomePages;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,8 @@ class GuestHomePagesController extends Controller
         $abouts = AboutPages::first();
         $heroWelcome = Hero::where('id', 1)->first();
         $testimonials = TestimonialHomePages::orderBy('id')->take(5)->get();
+        $portofolios  = PortofolioHomePages::get();
 
-        return view("welcome", compact('abouts', 'heroWelcome', 'testimonials'));
+        return view("welcome", compact('abouts', 'heroWelcome', 'testimonials', 'portofolios'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,67 +77,19 @@
                     </button>
                 </div>
                 <div class="carousel-container flex space-x-4 p-4">
-                    <div class="carousel-item flex-none w-1/4 bg-white p-4 rounded-lg">
-                        <div
-                            class="relative grid h-[40rem] w-full max-w-[28rem] flex-col items-end justify-center overflow-hidden rounded-xl bg-white bg-clip-border text-center text-gray-700">
+                    @foreach ($portofolios as $portofolio)
+                        <div class="carousel-item flex-none w-1/4 bg-white p-4 rounded-lg">
                             <div
-                                class="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-[url('https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')] bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
+                                class="relative grid h-[40rem] w-full max-w-[28rem] flex-col items-end justify-center overflow-hidden rounded-xl bg-white bg-clip-border text-center text-gray-700">
+                                <img src="{{ asset('uploads/' . $portofolio->thumbnail_portofolio) }}"
+                                    alt="{{ $portofolio->thumbnail_portofolio }}"
+                                    class="absolute inset-0 m-0 h-full w-full object-cover rounded-none shadow-none" />
                                 <div
                                     class="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50">
                                 </div>
                             </div>
-                            <div class="relative p-6 py-14 px-6 md:px-12">
-                                <h2
-                                    class="mb-6 block font-sans text-4xl font-medium leading-[1.5] tracking-normal text-white antialiased">
-                                    How we design and code open-source projects?
-                                </h2>
-                            </div>
                         </div>
-                    </div>
-                    <div class="carousel-item flex-none w-1/4 bg-white p-4 rounded-lg shadow-md">
-                        <h2 class="text-xl font-bold">Card 2</h2>
-                        <p class="text-gray-600">This is card number 2.</p>
-                    </div>
-                    <!-- Card 3 -->
-                    <div class="carousel-item flex-none w-1/4 bg-white p-4 rounded-lg shadow-md">
-                        <h2 class="text-xl font-bold">Card 3</h2>
-                        <p class="text-gray-600">This is card number 3.</p>
-                    </div>
-                    <!-- Card 4 -->
-                    <div class="carousel-item flex-none w-1/4 bg-white p-4 rounded-lg shadow-md">
-                        <h2 class="text-xl font-bold">Card 4</h2>
-                        <p class="text-gray-600">This is card number 4.</p>
-                    </div>
-                    <!-- Card 5 -->
-                    <div class="carousel-item flex-none w-1/4 bg-white p-4 rounded-lg shadow-md">
-                        <h2 class="text-xl font-bold">Card 5</h2>
-                        <p class="text-gray-600">This is card number 5.</p>
-                    </div>
-                    <!-- Card 6 -->
-                    <div class="carousel-item flex-none w-1/4 bg-white p-4 rounded-lg shadow-md">
-                        <h2 class="text-xl font-bold">Card 6</h2>
-                        <p class="text-gray-600">This is card number 6.</p>
-                    </div>
-                    <!-- Card 7 -->
-                    <div class="carousel-item flex-none w-1/4 bg-white p-4 rounded-lg shadow-md">
-                        <h2 class="text-xl font-bold">Card 7</h2>
-                        <p class="text-gray-600">This is card number 7.</p>
-                    </div>
-                    <!-- Card 8 -->
-                    <div class="carousel-item flex-none w-1/4 bg-white p-4 rounded-lg shadow-md">
-                        <h2 class="text-xl font-bold">Card 8</h2>
-                        <p class="text-gray-600">This is card number 8.</p>
-                    </div>
-                    <!-- Card 9 -->
-                    <div class="carousel-item flex-none w-1/4 bg-white p-4 rounded-lg shadow-md">
-                        <h2 class="text-xl font-bold">Card 9</h2>
-                        <p class="text-gray-600">This is card number 9.</p>
-                    </div>
-                    <!-- Card 10 -->
-                    <div class="carousel-item flex-none w-1/4 bg-white p-4 rounded-lg shadow-md">
-                        <h2 class="text-xl font-bold">Card 10</h2>
-                        <p class="text-gray-600">This is card number 10.</p>
-                    </div>
+                    @endforeach
                 </div>
                 <div class="absolute inset-y-0 right-0 flex items-center">
                     <button id="next" class="bg-blue-500 text-white p-2 rounded-full focus:outline-none">
@@ -150,7 +102,6 @@
         <section class="bg-white py-8 " id="portofolio">
             <h2 class="block mt-4 text-2xl font-semibold text-gray-800 hover:underline md:text-4xl text-center">Testimoni
             </h2>
-
             <div class="container mx-auto px-6">
                 <div class="relative">
                     <div class="flex overflow-x-auto scroll-smooth h-full scroll-container">
